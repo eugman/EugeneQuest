81092fd16ae144423723463fc08d93e3a897a537
Eugene Meidinger <eugene@eugenemeidinger.com>
2019-07-25 14:18:04 -0400

    Added check for if no databse exists. Converted hello world to database call.

--- app.py
+++ app.py
@@ -10,10 +10,22 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=False, nullable = False)
 
+class Daily(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable = False)
+    completed = db.Column(db.Boolean, nullable = False)
+    user = db.Integer, db.ForeignKey('user.id', nullable = False)
 
 @app.route('/')
 def index():
-    return "Hello world test!"
+    output = "<html><head></head><body><ul>"
+    dailies = Daily.query.all()
+    for daily in dailies:
+        output += "<li>"+daily.name+"</li>"
+
+    output +="</body></html>"
+
+    return output
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
--- resetDB.py
+++ resetDB.py
@@ -1,6 +1,11 @@
 import app
 import os
 
-os.remove("test.db")
+if os.path.exists("test.db"): 
+    os.remove("test.db")
 app.db.create_all()
+
+app.db.session.add(app.User(username="Eugene"))
+app.db.session.add(app.Daily(name="Check Blood Sugar", user = 1))
+app.db.session.commit()
 
