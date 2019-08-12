640b85bfc50d09f530933cec3afd15b19c66abe7
Eugene Meidinger <eugene@eugenemeidinger.com>
2019-08-12 15:07:27 -0400

    Added the ability to update goals.

--- routes/goals.py
+++ routes/goals.py
@@ -10,6 +10,14 @@ from flask_sqlalchemy import SQLAlchemy
 @app.route('/goals', methods=['GET', 'POST'])
 def goals():
     player = db.session.query(Player).get(1)
+    result = request.form
+
+    if result.get("id"):
+        goal_id = int(result.get("id"))
+        goal = db.session.query(Goal).get(goal_id)
+        goal.current = int(result.get("current"))
+
+        db.session.commit()
 
     goals = Goal.query.all()
     goals = sorted(goals, key = lambda x: float(x.level()), reverse = True)
